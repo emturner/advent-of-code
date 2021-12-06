9aa7ec4cee37285ea11d3c14a3b987ef1f9471af
Emma Turner <em.turner@tutanota.com>
2021-12-06 07:04:24 +0000

    solve 2021 day6

--- 2021/impls.scm
+++ 2021/impls.scm
@@ -6,6 +6,67 @@
   #:export (make-sub))
 
 ;; -----
+;; DAY 6
+;; -----
+(define-public (day6-part1)
+  (let* ((lines  (read-lines-to-list "day6"))
+         (fish  (parse-day6 (car lines)))
+         (after-80 (fish-step-days 80 fish)))
+    (count-fishes after-80)))
+
+(define-public (day6-part2)
+  (let* ((lines  (read-lines-to-list "day6"))
+         (fish  (parse-day6 (car lines)))
+         (after-80 (fish-step-days 256 fish)))
+    (count-fishes after-80)))
+
+(define-public (count-fishes current)
+  (hash-fold
+   (lambda (_days count acc)
+     (+ acc count))
+   0
+   current))
+
+(define-public (fish-step-days days start)
+  (if (> days 0)
+      (fish-step-days
+       (- days 1)
+       (fish-step-day start))
+      start))
+
+(define (fish-step-day fishes)
+  (let* ((new-table (make-hash-table))
+         (_ (hash-for-each
+             (lambda (days num)
+               (match days
+                 (0 (let* ((num-6 (or (hash-ref new-table 6) 0))
+                           (_ (hash-set! new-table 6 (+ num-6 num)))
+                           (_ (hash-set! new-table 8 (+ num-6 num))))
+                      #t))
+                 (7 (let* ((num-6 (or (hash-ref new-table 6) 0))
+                           (_ (hash-set! new-table 6 (+ num-6 num))))
+                      #t))
+                 (d (hash-set! new-table (- d 1) num))))
+             fishes)))
+    new-table))
+
+
+
+(define-public (parse-day6 input)
+  (let* ((numbers (string-split input #\,))
+         (numbers (map string->number numbers)))
+    (fold
+     (lambda (n acc)
+       (let ((entry (hash-ref acc n)))
+         (if entry
+             (let ((_ (hash-set! acc n (+ 1 entry))))
+               acc)
+             (let ((_ (hash-set! acc n 1)))
+               acc))))
+     (make-hash-table)
+     numbers)))
+
+;; -----
 ;; DAY 5
 ;; -----
 (define-public (day5-part1)
---
+++ 2021/tests/day6.scm
@@ -0,0 +1,63 @@
+(use-modules (impls)
+             (srfi srfi-64))
+
+(test-begin "day6")
+
+(define example-input "3,4,3,1,2")
+
+(define parsed-example-input
+  (let* ((h (make-hash-table))
+        (_  (hash-set! h 3 2))
+        (_  (hash-set! h 1 1))
+        (_  (hash-set! h 4 1))
+        (_  (hash-set! h 2 1)))
+    h))
+
+(test-equal "parse example input count"
+  4
+  (hash-count (lambda (_key _value) #t) (parse-day6 example-input)))
+
+(test-equal "parse example input 1 4s"
+  1
+  (hash-ref (parse-day6 example-input) 4))
+
+(test-equal "parse example input 1 2s"
+  1
+  (hash-ref (parse-day6 example-input) 2))
+
+(test-equal "parse example input 1 1"
+  1
+  (hash-ref (parse-day6 example-input) 1))
+
+(test-equal "parse example input 2 3s"
+  2
+  (hash-ref (parse-day6 example-input) 3))
+
+(test-equal "fishes count example after 1"
+  5
+  (count-fishes (fish-step-days 1 parsed-example-input)))
+
+(test-equal "fishes count example after 2"
+  6
+  (count-fishes (fish-step-days 2 parsed-example-input)))
+
+(test-equal "fishes count example after 3"
+  7
+  (count-fishes (fish-step-days 3 parsed-example-input)))
+
+(test-equal "fishes count example after 18"
+  26
+  (count-fishes (fish-step-days 18 parsed-example-input)))
+
+(test-equal "fishes count example after 80"
+  5934
+  (count-fishes (fish-step-days 80 parsed-example-input)))
+
+(test-equal "fishes count example after 256"
+  26984457539
+  (count-fishes (fish-step-days 256 parsed-example-input)))
+
+(test-equal "part 1" 390011 (day6-part1))
+
+
+(test-end "day6")
